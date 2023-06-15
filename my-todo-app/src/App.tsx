65a4572415d47e52a742b2d4ef9b3a66ef77e99c
6ixFeetWinter <104249266+6ixFeetWinter@users.noreply.github.com>
2023-06-15 11:28:58 +0900

    完成

--- my-todo-app/src/App.tsx
+++ my-todo-app/src/App.tsx
@@ -22,29 +22,29 @@ function App() {
     }
   }
 
+  const onDelete = (index:number) => {
+    const newMemos = [...memos];
+    newMemos.splice(index,1);
+    setMemos(newMemos);
+  }
+
   return (
-    <div>
+    <>
       <h1>MEMO APP</h1>
       <input type='text' placeholder='メモを入力してください' value={text} onChange={handleChange} />
       <button onClick={onAddMemo}>追加</button>
       <div>
         <ul>
-          <Items memos={memos} />
-          {/* propsでmemosを渡そうとしたらなんかエラーになった
-          <Items memos={memos} />
-          */}
-          {/* 型 'void[]' を型 'ReactNode' に割り当てることはできません。...
-          { memos.map((memo,index):void => {
-            <li key={index}>{memo}</li>
-          }) }
-        */}
-        {/* li生成されず。再レンダリングされていない...？ */}
-        {memos.map((memo,index):any => {
-          <li key={index}>{memo}</li>
-        })}
+          {/* <Items memos={memos} /> */}
+        {memos.map( (memo:string, index:number) => (
+          <li key={memo + index}>
+            <p>{memo}</p>
+            <button key={index} onClick={() => onDelete(index)}>削除</button>
+          </li>
+        ))}
         </ul>
       </div>
-    </div>
+    </>
   );
 };
 
--- my-todo-app/src/components/Items.tsx
+++ my-todo-app/src/components/Items.tsx
@@ -7,10 +7,8 @@ type ItemsProps = {
 export const Items: FC<ItemsProps> = (memos) => {
   console.log(memos)
   return (
-    <div>
-      {memos.map(( memo:string, index:number ) => {
-        <li key = {index}>{memo}</li>
-      })}
-    </div>
+    <ul>
+      
+    </ul>
   )
 }
